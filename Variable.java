4a7bc6848b13cb6829a22195674156841604843b
Zack <zclark0625@gmail.com>
2018-08-25 01:10:48 -0400

    test again

--- Variable.java
+++ Variable.java
@@ -4,7 +4,7 @@ public class Variable {
     public static void main(String[] arguments) {
         int tops;
         float gradePointAverage;
-        char key = 'X';
+        char key = 'Z';
         String productName = "Larvets";
     }
 }
