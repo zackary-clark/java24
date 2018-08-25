1f49380111c6d9b8c1b57731e2ee29dca4ad6fbf
Zack <zclark0625@gmail.com>
2018-08-25 00:46:30 -0400

    add variables to Variable class

--- Variable.java
+++ Variable.java
@@ -2,7 +2,10 @@ package com.java24hours;
 
 public class Variable {
     public static void main(String[] arguments) {
-        //Coming soon: variables
+        int tops;
+        float gradePointAverage;
+        char key = 'C';
+        String productName = "Larvets";
     }
 }
 
