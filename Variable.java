32c5f9459106a4eb166448b8f8d2014bf6841165
zclark0625 <41304364+zclark0625@users.noreply.github.com>
2018-08-25 00:47:36 -0400

    test

--- Variable.java
+++ Variable.java
@@ -4,7 +4,7 @@ public class Variable {
     public static void main(String[] arguments) {
         int tops;
         float gradePointAverage;
-        char key = 'C';
+        char key = 'X';
         String productName = "Larvets";
     }
 }
