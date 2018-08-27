cbd002a984acc7051f37d0f784c7a71e9dd30cf1
Zack <zclark0625@gmail.com>
2018-08-27 16:12:10 -0400

    Grade class

---
+++ Grade.java
@@ -0,0 +1,24 @@
+package com.java24hours;
+
+import java.util.*;
+
+public class Grade {
+    public static void main(String[] arguements) {
+        Random generator = new Random;
+        int grade = generator.nextInt(100);
+
+        if (grade < 60) {
+            System.out.println("F: " grade);
+        } else if (grade < 70) {
+            System.out.println("D: " + grade);
+        } else if (grade < 80) {
+            System.out.println("C: " + grade);
+        } else if (grade < 90) {
+            System.out.println("B: " + grade);
+        } else if (grade <= 100) {
+            System.out.println("A: " + grade);
+        } else {
+            System.out.println("What the hell happened?");
+        }
+    }
+}
