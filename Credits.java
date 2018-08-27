a591cf5a23458944f5673b828b75035361a73b5a
Zack <zclark0625@gmail.com>
2018-08-27 11:37:10 -0400

    Credits class

---
+++ Credits.java
@@ -0,0 +1,18 @@
+package com.java24hours;
+
+public class Credits {
+    public static void main(String[] arguments) {
+        String title = "Sharknado";
+        int year = 2013;
+        String director = "Anthony Ferrante";
+        String role1 = "Fin";
+        String actor1 = "Ian Ziering";
+        String role2 = "April";
+        String actor2 = "Tara Reid";
+        String role3 = "George";
+        String actor3 = "John Heard";
+        String role4 = "Nova";
+        String actor4 = "Cassie Scerbo";
+        System.out.println(title + " (" + year + ")\n" + "A " + director + " film.\n\n" + role1 + "\t" + actor1 + "\n" + role2 + “\t” + actor2 + “\n” + role3 + “\t” + actor3 + “\n” + role4 + “\t” + actor4);
+    }
+}
