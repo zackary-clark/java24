c61709c8c50906b77df4d0409f110951a7d31217
Zack <zclark0625@gmail.com>
2018-08-28 15:53:32 -0400

    Refactor

--- com/java24hours/Modem.java
+++ com/java24hours/Modem.java
@@ -1,11 +1,19 @@
 package com.java24hours;
 
 public class Modem {
-    int speed;
+    private int speed;
 
     public void displaySpeed() {
-        System.out.println("Speed: " + speed);
+        System.out.println("Speed: " + getSpeed());
     }
 
     public void disconnect() { System.out.println("Disconeecting ...");}
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 }
--- com/java24hours/ModemTester.java
+++ com/java24hours/ModemTester.java
@@ -4,8 +4,8 @@ public class ModemTester {
     public static void main(String[] arguments) {
         CableModem surfBoard = new CableModem();
         DslModem gateway = new DslModem();
-        surfBoard.speed = 500000;
-        gateway.speed = 400000;
+        surfBoard.setSpeed(500000);
+        gateway.setSpeed(400000);
         System.out.println("Trying the cable modem:");
         surfBoard.displaySpeed();
         surfBoard.connect();
